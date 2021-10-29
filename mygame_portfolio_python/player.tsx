f5ead2b7b8c53c5088b18265c88da7f9b63ebc20
Élodie Gagé <elodiegage@gmail.com>
2021-10-29 09:37:28 +0200

    Add files via upload
    added all sprites, sheets, .py docs, sorry need to clean this mess >.<

---
+++ mygame_portfolio_python/player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="player" tilewidth="16" tileheight="16" tilecount="72" columns="9">
+ <image source="player.png" trans="007575" width="153" height="128"/>
+</tileset>
---
+++ mygame_portfolio_python/scenario.py
@@ -0,0 +1,12 @@
+import pygame
+import pytmx
+from self import self
+
+#scenario :
+# go to house 1 = earn house1's bypass to go to house2,
+# house2 required house1's bypass = to earn house's2 bypass
+#house3 required house2's bypass = to complete quest.
+
+# if player go to house1 then directly house3= prompt message error
+# if player go to house2 first= prompt message error
+# if player go to house3 first= prompt message error
